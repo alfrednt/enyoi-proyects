6e6d29ae6cbda509c2663b9e4371825704cc3afb
alfrednt <alfred.nt@gmail.com>
2024-05-27 18:09:26 -0500

    realizando DOM

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-const section__profile = document.getElementById("section__profile");
+const certificateExpand = document.querySelectorAll('certificate');
+
+
+const expandCertificate = (event) => {
+    const element = event.target;
+    element.classList.add(
+        'h-[500px]',
+        'w-[800px]',
+        'duration-300'
+    );
+};
+
+
+certificateExpand.forEach(cert => {
+    cert.addEventListener('click', expandCertificate);
+});
