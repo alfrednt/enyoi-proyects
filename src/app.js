f4b9159bd110d72e76c8b3a47e50db6e1562707d
alfrednt <alfred.nt@gmail.com>
2024-05-30 19:32:50 -0500

    final dom listo

--- src/app.js
+++ src/app.js
@@ -10,3 +10,22 @@ const expandCertificate = (event) => {
 certificateExpand.forEach(cert => {
     cert.addEventListener('click', expandCertificate);
 });
+
+const menu_mobile = document.getElementById('menu_mobile');
+const menuShow = document.getElementById('menuShow');
+const  menuBarra = document.querySelectorAll('[menuBarra]')
+let isMenuOpen = false;
+const displayMenu = () => {
+   isMenuOpen = !isMenuOpen
+    menuShow.classList.toggle('flex');
+    if (isMenuOpen){
+        menuShow.classList.remove('hidden');
+        return
+    }
+    menuShow.classList.add("hidden");
+};
+menu_mobile.addEventListener('click', displayMenu);
+
+menuBarra.forEach(menuB => {
+    menuB.addEventListener('click', displayMenu);
+});
